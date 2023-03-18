52219eb506e2b3ea35b54aaf435d9e6fbb02bbb2
Saniokkk <kashirin.alexsandr91@gmail.com>
2023-03-18 20:42:04 +0200

    add homepage

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/courses/">
+    <BrowserRouter basename="/courses">
       <App />
     </BrowserRouter>
   </React.StrictMode>
